0a7db8ac367e1308aff37cb4fcfede485ce979dd
leonardo <leonardo.jimenezp@gmail.com>
2021-10-29 12:34:26 -0500

    link

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,17 @@
 import { User } from './../shared/user.interface';
 import { Injectable } from '@angular/core';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor( ) { }
-
+  constructor( ) { 
+   
+  }
 
 }
