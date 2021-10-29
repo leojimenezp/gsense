ed9e50a7a24b3e0689fc6f230744ef12e6773c46
leonardo <leonardo.jimenezp@gmail.com>
2021-10-29 07:49:03 -0500

    splash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,7 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+ 
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
@@ -18,6 +14,10 @@ const routes: Routes = [
   {
     path: 'signup',
     loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
+  {
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
   },
 ];
 
---
+++ src/app/splash/splash.page.ts
@@ -0,0 +1,22 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+})
+export class SplashPage implements OnInit {
+
+  constructor(
+    public router:Router
+  ) { 
+    setTimeout(()=>{
+      this.router.navigateByUrl('login');
+    },1000)
+  }
+
+  ngOnInit() {
+  }
+
+}
